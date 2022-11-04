c889168a1cb56ee61a6c051594a867cbc0615132
affandi2204 <m.affandi2016@gmail.com>
2022-11-04 10:28:04 +0700

    add storage url helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,10 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 if(!function_exists('processRules')) {
     function processRules($request, $rules, $messages = [], $attributes = [])
     {
         return Validator::make($request->all(), $rules, $messages, $attributes);
+    }
+}
+
+if(!function_exists('storageUrl')) {
+    function storageUrl($image)
+    {
+        return $image ? url('/').Storage::url($image) : null;;
     }
 }
